f8749ddddaa657f538c56923d6c614c2aa25acf7
StGrv <65493257+StGrv@users.noreply.github.com>
2023-01-24 11:08:50 +0200

    The config file for the dependancy injection container
    With this dependancy injection container we can switch wether we are calling the Book model methods or the Files model methods

--- web.php
+++ web.php
@@ -6,35 +6,17 @@ use app\modules\bookshop\interfaces\BookServiceInterface;
 require __DIR__ . '/globals.php';
 
 $config = [
-
     'container' => [
         'definitions' => [
             BookServiceInterface::class => [
                 'class' => BookService::class
             ],
             BookInterface::class => [
-                'class' => Book::class
-//                'class' => BookAdapter::class
+                'class' => Book::class // Can be switched to this one in orderr to use the Book model and its CRUD methods
+//                'class' => BookAdapter::class // Can be switched to this one in order to use the adapter which will call the Files model CRUD methods
             ],
         ]
     ]
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
-}
 
 return $config;
